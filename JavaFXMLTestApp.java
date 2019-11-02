5cbcb1a83bae6a01c477b3ad7846a066ca9f342c
RichardPham <richardhopham@gmail.com>
2019-11-01 18:31:54 -0700

    Added database connection code

--- JavaFXMLTestApp.java
+++ JavaFXMLTestApp.java
@@ -5,6 +5,11 @@
  */
 package javafxmltestapp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -26,11 +31,25 @@ public class JavaFXMLTestApp extends Application {
         stage.setScene(scene);
         stage.show();
     }
+    
+    public static void connect() throws SQLException { 
+    	String url = "jdbc:postgresql://database-1.cakeqdu3h1oe.us-west-1.rds.amazonaws.com/";
+		Properties props = new Properties();
+		props.setProperty("user","postgres");
+		props.setProperty("password","password");
+ 
+		Connection conn = DriverManager.getConnection(url, props);
+		String sqlStatement = "SELECT * FROM Public.Patients;";
+		conn.prepareStatement(sqlStatement).execute();
+		conn.close();
+    }
 
     /**
      * @param args the command line arguments
+     * @throws SQLException 
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+    	connect();
         launch(args);
     }
     
