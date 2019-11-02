e9af239083b24d4b58b414b2959231f17c811af1
Sebastian Hernandez <33647914+seabass189@users.noreply.github.com>
2019-11-01 18:04:14 -0700

    Files for first idea
    Not essential, can be replaced

---
+++ FXMLDocumentController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxmltestapp;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author sebas
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    @FXML
+    private Button button;
+    @FXML
+    private Button button2;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleButton2Action(ActionEvent event) {
+        System.out.println("You clicked me, button2!");
+        label.setText("Hello World 2!");
+    }
+    
+}
